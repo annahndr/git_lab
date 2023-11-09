e2bf355c420f8adf20bbd3788530488c8d7a5a76
Anna Henderson <annahenderson@gmail.com>
2023-11-09 10:09:28 +0000

    adds variable

--- Greet.java
+++ Greet.java
@@ -1,6 +1,7 @@
 public class Greet {
     public static void main(String[] args){
-        System.out.println("hello");
+        String greeting = "Good morning";
+        System.out.println(greeting);
         System.out.println("How are you?");
     }
 }
